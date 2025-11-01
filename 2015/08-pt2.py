81fefccb7094bc08949d94465b88f502b51503af
corned <oliver.kaarnalehto@gmail.com>
2025-11-01 21:19:39 +0200

    8 solved

---
+++ 2015/08-pt2.py
@@ -0,0 +1,13 @@
+with open("./08-input.txt", "r") as file:
+    lines = file.readlines()  # ['""', '"abc"', r'"aaa\"aaa"', r'"\x27"']
+    code_characters = sum([len(line.strip()) for line in lines])
+
+    s = 0
+    for line in lines:
+        line = line.strip()
+        quotes = line.count('"')
+        escapes = line.count("\\")
+        s += (len(line) - quotes - escapes) + 2 + (2 * quotes) + (2 * escapes)
+        # print(line, "->", len(line), sum)
+
+    print(s - code_characters)
---
+++ 2015/08.py
@@ -0,0 +1,8 @@
+with open("./08-input.txt", "r") as file:
+    lines = file.readlines()
+
+    code_characters = sum([len(line.strip()) for line in lines])
+    memory_characters = sum([len(eval(line.strip())) for line in lines])
+
+    print(code_characters, memory_characters)
+    print("answer:", code_characters - memory_characters)
