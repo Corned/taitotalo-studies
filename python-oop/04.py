a14133298e708178b02484ec9ff6754e1e4d7887
Corned <Corned@users.noreply.github.com>
2025-11-21 16:10:53 +0200

    renamed names

--- python-oop/04.py
+++ python-oop/04.py
@@ -1,15 +1,15 @@
 class Guessword:
     def __init__(self, word):
-        self.__secret_word = word
+        self.__answer_word = word
         self.guessed_letters = {}
 
-    def get_hidden_word(self) -> str:
+    def generate_hidden_word(self) -> str:
         return "".join(
-            [c if c in self.guessed_letters else "_" for c in self.__secret_word]
+            [c if c in self.guessed_letters else "_" for c in self.__answer_word]
         )
 
     def is_resolved(self) -> bool:
-        return self.__secret_word == self.get_hidden_word()
+        return self.__answer_word == self.generate_hidden_word()
 
     def resolve_letter(self, letter: str):
         self.guessed_letters[letter] = True
@@ -18,8 +18,8 @@ class Guessword:
 game = Guessword("kuningas")
 
 while not game.is_resolved():
-    print(game.get_hidden_word())
+    print(game.generate_hidden_word())
     letter = input("Kirjain? ")
     game.resolve_letter(letter)
 
-print("Voitit!", game.get_hidden_word())
+print("Voitit!", game.generate_hidden_word())
